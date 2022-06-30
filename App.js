cf4c15a660ca16586b55c3b517414f30b3613358
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-06-29 20:47:47 -0400

    upfated formatting

--- App.js
+++ App.js
@@ -20,20 +20,20 @@ class ReactApp extends React.Component {
   //==================== Check Decimal Functions ==============================>
   // resets decimal state
   resetDecimal = () => {
-    this.setState({hasDecimal: false})
+    this.setState({ hasDecimal: false })
   }
   // Add decimal to inputNumbers array if decimal is not present in number
   enterDecimal = (num) => {
     if (this.state.hasDecimal === false) {
-      this.setState({ inputNumbers: [...this.state.inputNumbers, num]});
-      }
+      this.setState({ inputNumbers: [...this.state.inputNumbers, num] });
+    }
   }
   // checks to see if decimal in number. returns TRUE if decimal present
   checkDecimal = (num) => {
     // if number has no decimal enter decimal and change state to "hasDecimal"
     if (num === "." && this.state.hasDecimal === false) {
       this.enterDecimal(num);
-      this.setState({hasDecimal: true})
+      this.setState({ hasDecimal: true })
     }
   }
 
@@ -42,9 +42,9 @@ class ReactApp extends React.Component {
     // stores length of inputNumbers array
     var inputNumLength = this.state.inputNumbers.length;
     // created index of last input
-    var index = inputNumLength -1;
+    var index = inputNumLength - 1;
     // index value of last input value
-    var lastInput = this.state.inputNumbers[index] 
+    var lastInput = this.state.inputNumbers[index]
     var beforeLast = this.state.inputNumbers[index - 1]
 
     // makes sure "*", "/", "-", "+", "." are legal inputs for calculator,
@@ -52,70 +52,70 @@ class ReactApp extends React.Component {
     switch (num) {
       case '*':
         this.resetDecimal();
-        if(lastInput === "*" || lastInput === "/" || lastInput === "-" || lastInput === "+") {
-        }else {
-          this.setState({ inputNumbers: [...this.state.inputNumbers, num]})
+        if (lastInput === "*" || lastInput === "/" || lastInput === "-" || lastInput === "+") {
+        } else {
+          this.setState({ inputNumbers: [...this.state.inputNumbers, num] })
         }
         break;
 
       case '/':
         this.resetDecimal();
-        if(lastInput === "*" || lastInput === "/" || lastInput === "-" || lastInput === "+") {
-        }else {
-          this.setState({ inputNumbers: [...this.state.inputNumbers, num]})
+        if (lastInput === "*" || lastInput === "/" || lastInput === "-" || lastInput === "+") {
+        } else {
+          this.setState({ inputNumbers: [...this.state.inputNumbers, num] })
         }
         break;
 
       case '+':
         this.resetDecimal();
-        if(lastInput === "*" || lastInput === "/" || lastInput === "-" || lastInput === "+") { 
-        }else {
-          this.setState({ inputNumbers: [...this.state.inputNumbers, num]})
+        if (lastInput === "*" || lastInput === "/" || lastInput === "-" || lastInput === "+") {
+        } else {
+          this.setState({ inputNumbers: [...this.state.inputNumbers, num] })
         }
         break;
 
       case '-':
         this.resetDecimal();
-        if(lastInput === "-") {          
-        }else {
-          this.setState({ inputNumbers: [...this.state.inputNumbers, num]})
+        if (lastInput === "-") {
+        } else {
+          this.setState({ inputNumbers: [...this.state.inputNumbers, num] })
         }
         break;
 
-      case ".": 
+      case ".":
         this.checkDecimal(num); // ==> Line 65  ==> enterDecimal() Line 69
-      break;
+        break;
 
       case 0:
-        if(num === 0 && this.state.inputNumbers[0] === 0 && inputNumLength === 1) {
+        if (num === 0 && this.state.inputNumbers[0] === 0 && inputNumLength === 1) {
           return false;
-        }else {
-          this.setState({ inputNumbers: [...this.state.inputNumbers, num]});
+        } else {
+          this.setState({ inputNumbers: [...this.state.inputNumbers, num] });
         }
         break;
 
       default:
         console.log("Switch Default Action, Input = " + num);
-        this.setState({ inputNumbers: [...this.state.inputNumbers, num]});
+        this.setState({ inputNumbers: [...this.state.inputNumbers, num] });
     }
   }
 
   //==================== Clear Functions ==================================>
   // These function clear state to initial satte when initialized
   clearOperator = () => {
-    this.setState({opSign: ""});
+    this.setState({ opSign: "" });
   }
   clearInputNumbers = () => {
-    this.setState({ inputNumbers: []});
+    this.setState({ inputNumbers: [] });
   }
   clearNumList = () => {
-    this.setState({ numList: []});
+    this.setState({ numList: [] });
   }
   clearCalculation = () => {
-    this.setState({calculation: ""});
+    this.setState({ calculation: "" });
   }
   clearCalculationList = () => {
-    this.setState({calculationList: []});
+    this.setState({ calculationList: [] });
   }
   clearScreen = () => {
     this.clearNumList();
@@ -129,25 +129,25 @@ class ReactApp extends React.Component {
   enterNumber = (input) => {
     //passes input to check functions to see if input is valid.
     this.checkNumber(input); //checkNumber() ==> Line 84 <==
-    
+
   }
 
   //==================== Calculate Function ===================================>
   calculate = () => {
     var inputNumLength = this.state.inputNumbers.length;  // stores length of inputNumbers array
-    var index = inputNumLength -1;                        // created index of last input
+    var index = inputNumLength - 1;                       // created index of last input
     var lastInput = this.state.inputNumbers[index];       // index value of last input value
     var beforeLast = this.state.inputNumbers[index - 1]   // index value before last input
     var firstInput = this.state.inputNumbers[0];          // index value of first input 
 
     // if last input is not a number... DO NOTHING
-    if(lastInput === "*" || lastInput === "/" || lastInput === "-" || lastInput === "+") {
+    if (lastInput === "*" || lastInput === "/" || lastInput === "-" || lastInput === "+") {
       alert("enter a number after the operator!!")
 
       // else if first input is not a number do this...
-    } else if(firstInput === "*" || firstInput === "/" || firstInput === "+") {
+    } else if (firstInput === "*" || firstInput === "/" || firstInput === "+") {
       // if number has been calculated previously...use operator on that number.
-      if(this.state.calculation != "") {
+      if (this.state.calculation != "") {
         var inputArray = this.state.inputNumbers;           // stores inputNumbers state into variable 
         inputArray.shift()                                  // removes the operator from the value
         var calculatedInput = eval(inputArray.join(""));    // calculates value and stores in new variable
@@ -160,41 +160,41 @@ class ReactApp extends React.Component {
             var calculationOutput = calculatedInput * savedCalculation;
             this.setState((state, props) => ({
               calculation: calculationOutput,
-              }))
+            }))
             break;
-    
+
           case '/':
             var calculationOutput = savedCalculation / calculatedInput;
             this.setState((state, props) => ({
               calculation: calculationOutput,
-              }))
+            }))
             break;
-    
+
           case '+':
             var calculationOutput = savedCalculation + calculatedInput;
             this.setState((state, props) => ({
               calculation: calculationOutput,
-              }))
+            }))
             break;
-    
+
           case '-':
             var calculationOutput = savedCalculation - calculatedInput;
             this.setState((state, props) => ({
               calculation: calculationOutput,
-              }))
+            }))
             break;
           default:
             console.log("WIERD ERROR");
         }
       }
 
-    // else if any positive number after first calculation do this..
-    } else if(this.state.calculation.length < 1) {
+      // else if any positive number after first calculation do this..
+    } else if (this.state.calculation.length < 1) {
       var calculatedInput = eval(this.state.inputNumbers.join(""))
-      this.setState({calculation: calculatedInput}) 
+      this.setState({ calculation: calculatedInput })
 
-    // else if number is negative do this...
-    } else if(firstInput == "-") {
+      // else if number is negative do this...
+    } else if (firstInput == "-") {
       var inputArray = this.state.inputNumbers;           // stores inputNumbers state into variable 
       inputArray.shift()                                  // removes the operator from the value
       var calculatedInput = eval(inputArray.join(""));    // calculates value and stores in new variable
@@ -203,41 +203,40 @@ class ReactApp extends React.Component {
       var calculationOutput = savedCalculation - calculatedInput;
       this.setState((state, props) => ({
         calculation: calculationOutput,
-        }))
-      
-    // else start a new number if no beginning operator    
+      }))
+
+      // else start a new number if no beginning operator    
     } else {
       var calculatedInput = eval(this.state.inputNumbers.join(""));
 
       // === ==> CONSOLE LOGS for troubleshooting and debugging <== ===
       console.log("savedCalculation = " + savedCalculation)
       console.log("calculatedInput = " + calculatedInput)
-      console.log("calculationOutput = "+ calculationOutput)
+      console.log("calculationOutput = " + calculationOutput)
 
       this.setState((state, props) => ({
-      calculation: calculatedInput,
+        calculation: calculatedInput,
       }))
     }
   }
 
   //================= Main Equal Function =====================================>
   getCalculation = () => {
-    if(this.state.inputNumbers.length > 0) { 
-    this.calculate();          // ==> Line 180 <== 
-    this.clearOperator();      // ==> Line 148 <==
-    this.clearInputNumbers();  // ==> Line 151 <==
-    this.resetDecimal();       // ==> Line 65  <==
+    if (this.state.inputNumbers.length > 0) {
+      this.calculate();          // ==> Line 180 <== 
+      this.clearOperator();      // ==> Line 148 <==
+      this.clearInputNumbers();  // ==> Line 151 <==
+      this.resetDecimal();       // ==> Line 65  <==
     }
   }
 
-  
   render() {
     // Console logs for troubleshooting and debugging
     console.log("calculation " + this.state.calculation)
     console.log("inputNumbers = " + this.state.inputNumbers);
     console.log("numList = " + this.state.numList);
     console.log("=================================");
-    
+
     return (
       <div >
         <h1>Calculator App</h1>
@@ -248,59 +247,59 @@ class ReactApp extends React.Component {
             <div id="arithmetic-display">
               {this.state.calculation}
             </div>
-            <div id="display"> 
+            <div id="display">
               {this.state.inputNumbers}
             </div>
           </div>
 
-            <div id="key-pad">
-              <div id="num-container">
-                <div id="num-row-4">
-                  <div id="seven" className="button" onClick={() => {this.enterNumber(7)}}> 7 </div>
-                  <div id="eight" className="button" onClick={() => {this.enterNumber(8)}}> 8 </div>
-                  <div id="nine" className="button"  onClick={() => {this.enterNumber(9)}}> 9 </div>
-                </div>
-
-                <div id="num-row-3">
-                  <div id="four" className="button"  onClick={() => {this.enterNumber(4)}}> 4 </div>
-                  <div id="five" className="button"  onClick={() => {this.enterNumber(5)}}> 5 </div>
-                  <div id="six" className="button"   onClick={() => {this.enterNumber(6)}}> 6 </div>
-                </div>
-
-                <div id="num-row-2">
-                  <div id="one" className="button"   onClick={() => {this.enterNumber(1)}}> 1 </div>
-                  <div id="two" className="button"   onClick={() => {this.enterNumber(2)}}> 2 </div>
-                  <div id="three" className="button" onClick={() => {this.enterNumber(3)}}> 3 </div>
-                </div>
-
-                <div id="num-row-1">
-                  {/* Numbers */}
-                  <div id="zero" className="button"  onClick={() => {this.enterNumber(0)}}> 0 </div>
-                </div>
-
-                <div id="num-row-0">
-                  {/* Clear */}
-                  <div id="clear" className="button" onClick={() => {this.clearScreen()}}> AC </div>
-                  {/* Equals */}
-                  <div id="equals" className="button" onClick={() => {this.getCalculation()}}> = </div>
-                </div>
+          <div id="key-pad">
+            <div id="num-container">
+              <div id="num-row-4">
+                <div id="seven" className="button" onClick={() => { this.enterNumber(7) }}> 7 </div>
+                <div id="eight" className="button" onClick={() => { this.enterNumber(8) }}> 8 </div>
+                <div id="nine" className="button" onClick={() => { this.enterNumber(9) }}> 9 </div>
               </div>
 
-              <div id="calculation-operators">
-                {/* Arithmetic Operators */}
-                <div id="add" className="button"      onClick={() => {this.enterNumber("+")}}> + </div>
-                <div id="subtract" className="button" onClick={() => {this.enterNumber("-")}}> - </div>
-                <div id="multiply" className="button" onClick={() => {this.enterNumber("*")}}> × </div>
-                <div id="divide" className="button"   onClick={() => {this.enterNumber("/")}}> ÷ </div>
-                {/* Decimal */}
-                <div id="decimal" className="button"  onClick={() => {this.enterNumber(".")}}> . </div>
+              <div id="num-row-3">
+                <div id="four" className="button" onClick={() => { this.enterNumber(4) }}> 4 </div>
+                <div id="five" className="button" onClick={() => { this.enterNumber(5) }}> 5 </div>
+                <div id="six" className="button" onClick={() => { this.enterNumber(6) }}> 6 </div>
+              </div>
+
+              <div id="num-row-2">
+                <div id="one" className="button" onClick={() => { this.enterNumber(1) }}> 1 </div>
+                <div id="two" className="button" onClick={() => { this.enterNumber(2) }}> 2 </div>
+                <div id="three" className="button" onClick={() => { this.enterNumber(3) }}> 3 </div>
+              </div>
+
+              <div id="num-row-1">
+                {/* Numbers */}
+                <div id="zero" className="button" onClick={() => { this.enterNumber(0) }}> 0 </div>
+              </div>
+
+              <div id="num-row-0">
+                {/* Clear */}
+                <div id="clear" className="button" onClick={() => { this.clearScreen() }}> AC </div>
+                {/* Equals */}
+                <div id="equals" className="button" onClick={() => { this.getCalculation() }}> = </div>
               </div>
             </div>
+
+            <div id="calculation-operators">
+              {/* Arithmetic Operators */}
+              <div id="add" className="button" onClick={() => { this.enterNumber("+") }}> + </div>
+              <div id="subtract" className="button" onClick={() => { this.enterNumber("-") }}> - </div>
+              <div id="multiply" className="button" onClick={() => { this.enterNumber("*") }}> × </div>
+              <div id="divide" className="button" onClick={() => { this.enterNumber("/") }}> ÷ </div>
+              {/* Decimal */}
+              <div id="decimal" className="button" onClick={() => { this.enterNumber(".") }}> . </div>
+            </div>
+          </div>
         </div>
       </div>
     );
   }
-}       
+}
 
 root.render(
   <div>
